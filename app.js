629d4faedf7ed99e10097520bd00c73c585bb482
Charlotte Strickland <charlottemarie81@gmail.com>
2022-05-25 19:24:17 -0400

    updated app.js with new information for game

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 class Casino {
     // Write code here
-};
+    constructor(name, fakeCoin) {
+        this.name = name;
+        this.timesPlayed = 0;
+        this.winnings = 0;
+        this.fakeCoin = fakeCoin;
+    }
+
+
+playGame(betAmount) {
+    this.timesPlayed += 1; 
+    if (Math.random() > 0.5 && this.fakeCoin == false) {
+        console.log("You won " + betAmount *
+        (this.timesPlayed) + " dollars from " + this.name + "!");
+        this.winnings += this.timesPlayed * betAmount;
+        console.log("Current winnings: " + this.winnings);
+    } else {
+        console.log(this.name + " wins!");
+        this.timesPlayed = 0;
+        this.winnings = 0;
+    }
+}
+
+rollDice(sides){
+    console.log(Math.floor(Math.random() * sides) + 1);
+    }
+}
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
+const myCasino = new Casino("HackerU Casino", false);
 console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
 myCasino.playGame(25);
 myCasino.playGame(35);
+
 
 // BONUS TESTS
 /*
